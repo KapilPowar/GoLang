8b1a85aa6a53098beb3ccc9908193f528efe2f0c
Kapil Powar <kapil.powar@in.ibm.com>
2021-07-26 15:23:49 +0530

    closure in go

--- 03-functions/04-demo.go
+++ 03-functions/04-demo.go
@@ -5,25 +5,26 @@ import "fmt"
 func main() {
 	x := 10
 	y := 20
-	z := 30
+	//z := 30
 	// addResult := add(x, y)
 	// fmt.Printf("Result of proccessing %d & %d is %d\n", x, y, addResult)
 	// subtractResult := sub(x, y)
 	// fmt.Printf("Result of proccessing %d & %d is %d\n", x, y, subtractResult)
-	//compute(x, y, add)
-	compute(x, y, z, add)
+
+	compute(x, y, add)
+	compute(x, y, sub)
 }
 
-func add(x, y, z int) int {
-	return x + y + z
+func add(x, y int) int {
+	return x + y
 }
 
 func sub(x, y int) int {
 	return x - y
 }
 
-func compute(x, y, z int, fn func(int, int, int) int) {
-	result := fn(x, y, z)
-	fmt.Printf("Result of proccessing is %d\n", result)
+func compute(x, y int, fn func(int, int) int) {
+	result := fn(x, y)
+	fmt.Printf("Result of proccessing %d & %d is %d\n", x, y, result)
 
 }
