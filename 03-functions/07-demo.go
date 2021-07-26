7fbe2ed7c8893816a664fd374b216a434c03203a
Kapil Powar <kapil.powar@in.ibm.com>
2021-07-26 16:28:49 +0530

    state in deferred function

---
+++ 03-functions/07-demo.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/* defer func() {
+		fmt.Println("Exiting from main")
+	}() */
+	defer fmt.Println("Exiting from main")
+	fmt.Println("Main started")
+	f1()
+	fmt.Println("Main completed")
+}
+
+func f1() {
+	defer fmt.Println("[defer - df1] exiting from f1")
+	defer fmt.Println("[defer - df2] exiting from f1")
+	defer fmt.Println("[defer - df3] exiting from f1")
+	fmt.Println("f1 invoked")
+	msg := f2()
+	fmt.Println("f1 received message:", msg)
+}
+
+func f2() (message string) {
+	data := 100
+	defer func() {
+		fmt.Println("[defer - df1] exiting from f2")
+		message = fmt.Sprintf("message from defer-df1 of f2 func with data %d", data)
+	}()
+	defer fmt.Println("[defer - df2] exiting from f2")
+	defer fmt.Println("[defer - df3] exiting from f2")
+	fmt.Println("f2 invoked")
+	data = 200
+	message = "message from the f2 func"
+	return
+}
