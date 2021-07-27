4ff715dc616b28b6a4f3fa6140a799adec67a8b3
Kapil Powar <kapil.powar@in.ibm.com>
2021-07-27 17:18:41 +0530

    Sort

---
+++ 08-interfaces/models/products.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Products []Product
+
+func (ps Products) Print() {
+	for _, val := range ps {
+		fmt.Println(val)
+	}
+}
+
+/* implement the sort apis */
+func (ps Products) Len() int           { return len(ps) }
+func (ps Products) Less(i, j int) bool { return ps[i].Id < ps[j].Id }
+func (ps Products) Swap(i, j int)      { ps[i], ps[j] = ps[j], ps[i] }
+
+func (ps Products) Sort() {
+	sort.Sort(ps)
+}
+
+type byName struct {
+	Products
+}
+
+func (ps byName) Less(i, j int) bool {
+	return ps.Products[i].Name < ps.Products[j].Name
+}
+
+func (ps Products) SortByName() {
+	sort.Sort(byName{ps})
+}
+
+type byCost struct {
+	Products
+}
+
+func (ps byCost) Less(i, j int) bool {
+	return ps.Products[i].Cost < ps.Products[j].Cost
+}
+
+func (ps Products) SortByCost() {
+	sort.Sort(byCost{ps})
+}
---
+++ 08-interfaces/program.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"interfaces-app/models"
+)
+
+func main() {
+	// c := shapes.Circle{Radius: 3}
+	// fmt.Println(c)
+	// /* utils.PrintArea(c)
+	// utils.PrintPerimeter(c) */
+	// utils.PrintDimension(c)
+
+	// r := shapes.Rectangle{Width: 10, Height: 12}
+	// fmt.Println(r)
+	// /* utils.PrintArea(r)
+	// utils.PrintPerimeter(r) */
+	// utils.PrintDimension(r)
+
+	products := models.Products{
+		models.Product{105, "Pen", 5, 50, "Stationary"},
+		models.Product{103, "Pencil", 2, 100, "Stationary"},
+		models.Product{102, "Marker", 50, 20, "Stationary"},
+		models.Product{104, "Frying Pan", 500, 5, "Utencil"},
+		models.Product{101, "Phone", 5000, 3, "Electronics"},
+		models.Product{100, "Bowl", 100, 50, "Utencil"},
+	}
+
+	/*
+		using the sort apis implemented to sort the products by id, name, cost etc and print the result
+	*/
+	fmt.Println("========= Initial List ==============")
+	products.Print()
+	/*
+		using the sort apis implemented to sort the products by id, name, cost etc and print the result
+	*/
+	fmt.Println("========= Default Sort ==============")
+	products.Sort()
+	products.Print()
+
+	fmt.Println("========= Sort By Name ==============")
+	products.SortByName()
+	products.Print()
+	fmt.Println("========= Sort By Cost ==============")
+	products.SortByCost()
+	products.Print()
+
+}
