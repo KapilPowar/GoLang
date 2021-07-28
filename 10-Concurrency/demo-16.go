333b2de488a0f80b009d96c17fb4521012ea2148
Kapil Powar <kapil.powar@in.ibm.com>
2021-07-28 15:44:40 +0530

    timer APIs

---
+++ 10-Concurrency/demo-16.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	go fibonacci(ch)
+
+	for no := range ch {
+		println(no)
+	}
+
+	fmt.Println("Done")
+}
+
+func fibonacci(ch chan int) {
+	stop := time.After(5 * time.Second)
+	x, y := 0, 1
+	for {
+		select {
+		case ch <- x:
+			time.Sleep(500 * time.Millisecond)
+			x, y = y, x+y
+		case <-stop:
+			fmt.Println("Stopped")
+			close(ch)
+			return
+		}
+	}
+}
